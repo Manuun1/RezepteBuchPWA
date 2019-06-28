aa64144b000078625b15f3d576408e157ddfd242
Max OSX <>
2019-06-28 17:11:43 +0200

    Added Angular Material

---
+++ src/app/angular-material.ts
@@ -0,0 +1,48 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule, MatCheckboxModule } from "@angular/material";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatChipsModule,
+    MatDialogModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatSidenavModule,
+    MatChipsModule,
+    MatDialogModule
+  ]
+})
+export class angular_material {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//Using my own custom file for Angular Material Component Imports 
+import {angular_material} from "./angular-material";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    angular_material
   ],
   providers: [],
   bootstrap: [AppComponent]
