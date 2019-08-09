d47314d4a21644a1aeb8f619753728be47eda33a
Max OSX <>
2019-08-09 13:18:38 +0200

    Add Recipe Rework Converting data input to Two Way Binding in Angular

--- src/app/recipies/add-recipe/add-recipe.component.ts
+++ src/app/recipies/add-recipe/add-recipe.component.ts
@@ -5,7 +5,13 @@ import {
   Validators,
   FormControl
 } from "@angular/forms";
-import { DataService, difficulty, ingredient,recipe, ingredient_amount } from "src/app/data.service";
+import {
+  DataService,
+  difficulty,
+  ingredient,
+  recipe,
+  ingredient_amount
+} from "src/app/data.service";
 import { Router } from "@angular/router";
 import { Observable, empty } from "rxjs";
 import { map, startWith } from "rxjs/operators";
@@ -22,25 +28,42 @@ export interface ingredient_with_amount {
   styleUrls: ["./add-recipe.component.css"]
 })
 export class AddRecipeComponent implements OnInit {
+  myControl = new FormControl();
   isLinear = false;
+
+  //Form Controls
   form_basics: FormGroup;
   form_ingredients: FormGroup;
   form_thirdFormGroup: FormGroup;
 
+  //Caching Data from DataService
   difficulties: difficulty[];
   ingredients: ingredient[];
 
-  myControl = new FormControl();
+  //Two Way Binding Variables
+  Zutat: any;
+  recipeName = "";
+  recipeImageLink = "";
+  difficulty:any;
+  description = "";
+  requiredTime = 0;
+
   filteredOptions: Observable<ingredient[]>;
+  selected_ingredient: ingredient = {
+    name: "select an ingredient",
+    unit: "select an ingredient",
+    category: "select an ingredient"
+  };
+
   selected_ingredients: ingredient_with_amount[] = [];
-  anzahl:number;
+  anzahl: number;
 
-  error_code:string;
+  error_code: string;
 
-  new_recipe:recipe;
+  new_recipe: recipe;
 
-  display_stepper:boolean = true;
-  insert_successfull:boolean = false;
+  display_stepper: boolean = true;
+  insert_successfull: boolean = false;
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -50,22 +73,19 @@ export class AddRecipeComponent implements OnInit {
 
   ngOnInit() {
     this.difficulties = this.dataservice.get_difficulties();
-    this.dataservice.get_allIngredients1DArray().subscribe(item => this.ingredients=item);
-
-    console.log(this.ingredients);
-
-    /*item.forEach(function(category) {
-      category.data.forEach(function(ing){
-        console.log(ing);
-        this.ingredients.push(ing as ingredient);
-      }).bind(this.ingredients)*/
+    this.dataservice
+      .get_allIngredients1DArray()
+      .subscribe(item => (this.ingredients = item));
 
     this.form_basics = this._formBuilder.group({
-      Rezeptname: ["", Validators.required]
+      recipeName: ["", Validators.required],
+      difficulty: ["", Validators.required],
+      recipeImageLink: ["", Validators.required]
     });
     this.form_ingredients = this._formBuilder.group({});
     this.form_thirdFormGroup = this._formBuilder.group({
-      thirdCtrl: ["", Validators.required]
+      description: ["", Validators.required],
+      requiredTime: ["", Validators.required]
     });
 
     this.filteredOptions = this.myControl.valueChanges.pipe(
@@ -76,6 +96,8 @@ export class AddRecipeComponent implements OnInit {
   }
 
   displayFn(ingredient?: ingredient): string | undefined {
+    this.selected_ingredient = ingredient;
+    console.log(this.difficulties);
     return ingredient ? ingredient.name : undefined;
   }
 
@@ -87,13 +109,12 @@ export class AddRecipeComponent implements OnInit {
   }
 
   add_ingredient(zutat: any, anzahl: any) {
-
     console.log(this.anzahl);
 
     let found: boolean = false;
 
     if (zutat != "" && anzahl / 2 != 0) {
-      this.error_code="";
+      this.error_code = "";
       this.selected_ingredients.forEach(function(element) {
         if (element.name == zutat) {
           found = true;
@@ -106,55 +127,47 @@ export class AddRecipeComponent implements OnInit {
           unit: this.dataservice.get_ingredient_data(zutat).unit
         });
       }
-    }
-    else{
+    } else {
       this.error_code = "Bitte sowohl eine Zutat als auch eine Anzahl angeben.";
     }
   }
 
-  generate_recipe_summary(){
-    console.log("Recipe summary");
-    this.selected_ingredients.forEach(function(element){
-      //this.recipe_summary+=element.
-    })
-  }
+  add_recipe() {
+    let ingredient_amount: ingredient_amount[] = [];
+    let difficulty: difficulty;
 
-  add_recipe(recipe_name:string,selected_difficulty: number,description:string,duration:number,recipe_image_link:string) {
+    this.selected_ingredients.forEach(function(element) {
+      ingredient_amount.push({ key: element.name, amount: +element.amount });
+    });
 
-    let ingredient_amount:ingredient_amount[] = [];
-    let difficulty:difficulty;
+    console.log(this.difficulty);
+    console.log(this.recipeName);
 
-    this.selected_ingredients.forEach(function(element){
-      ingredient_amount.push({key:element.name,amount:+element.amount});
-    })
-
-    difficulty = this.dataservice.get_difficulties_byValue(selected_difficulty);
+    difficulty = this.dataservice.get_difficulties_byValue(this.difficulty);
     console.log(difficulty);
-    
-    if(difficulty!=null)
-    {
+
+    if (difficulty != null) {
       this.new_recipe = {
-        name:recipe_name,
-        ingredients:  ingredient_amount,
-        required_time: duration,
-        description:description,
-        difficulty:difficulty,
-        image:recipe_image_link
-      }
+        name: this.recipeName,
+        ingredients: ingredient_amount,
+        required_time: this.requiredTime,
+        description: this.description,
+        difficulty: difficulty,
+        image: this.recipeImageLink
+      };
       let response = this.dataservice.add_recipe(this.new_recipe);
       console.log("recipe was added");
-      
-      if(response == true)
-      {
+
+      if (response == true) {
         console.log("adding was successful");
-        
-        this.display_stepper=false;
-        this.insert_successfull=true;
+
+        this.display_stepper = false;
+        this.insert_successfull = true;
       }
     }
   }
 
-  routeToRecipeView(){
+  routeToRecipeView() {
     this.router.navigate(["/"]);
   }
 }
